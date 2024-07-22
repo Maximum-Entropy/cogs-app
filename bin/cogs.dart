d32283af8940cfea486bf384ce086288843c5b34
nsprute <niklas.sprute@mpg.tuebingen.de>
2024-07-22 16:43:32 +0200

    dart create

---
+++ bin/cogs.dart
@@ -0,0 +1,5 @@
+import 'package:cogs/cogs.dart' as cogs;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cogs.calculate()}!');
+}
---
+++ test/cogs_test.dart
@@ -0,0 +1,8 @@
+import 'package:cogs/cogs.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
